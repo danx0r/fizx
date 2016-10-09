546eeae68ea3ff81ea990e9a1ad77b897341d7a9
danx0r <danbmil99@gmail.com>
2016-10-09 11:30:31 -0700

    big meet small

--- fizx.js
+++ fizx.js
@@ -380,17 +380,17 @@ function asx(url, cb) {
 }
 
 test3 = function() {
-  RADIUS = 6
+  RADIUS = 11
   DAMP = 1
   BOND_P = 150
   BOND_D = .05
-  REALTIME = .2; TICK_SHOW=TICK_PHYS
+  // REALTIME = .2; TICK_SHOW=TICK_PHYS
   display_init();
   asx("./ball60.json", function() {
     var ball1 = new thing("ball1", 800, 200, -300, 0, JSON.parse(this.responseText));
     bond_triangulate(ball1.atoms, true);
-    asx("./ball60.json", function() {
-      var ball2 = new thing("ball2", 300, 420, 300, 0, JSON.parse(this.responseText));
+    asx("./ball16.json", function() {
+      var ball2 = new thing("ball2", 300, 220, 300, 0, JSON.parse(this.responseText));
       bond_triangulate(ball2.atoms, true);
       COLLIDES.push([ball1, ball2]);
       console.log(BONDS.length, "bonds")
