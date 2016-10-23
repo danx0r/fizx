d31e21033d71050b8ee590886dcfabacdc450f7f
danx0r <danbmil99@gmail.com>
2016-10-23 15:29:09 -0700

    HOST arg for server

--- servit.py
+++ servit.py
@@ -1,8 +1,14 @@
 import SimpleHTTPServer
 import SocketServer
 PORT = 8002
+HOST = ""
+import sys
+if len(sys.argv) > 1:
+    HOST = sys.argv[1]
+if len(sys.argv) > 2:
+    PORT = int(sys.argv[2])
 Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
 SocketServer.TCPServer.allow_reuse_address = True
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
+httpd = SocketServer.TCPServer((HOST, PORT), Handler)
+print "serving at %s:%s" % (HOST, PORT)
 httpd.serve_forever()
