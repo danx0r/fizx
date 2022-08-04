8bc4fecba5e8c53c4fa81096d505959e51a7cb31
danx0r <dan.miller@eye0.com>
2022-08-04 13:57:19 -0700

    cleanup

--- fizx.js
+++ fizx.js
@@ -142,23 +142,12 @@ fizxlib = function() {
   
     var dvx = b.v.x-a.v.x;
     var dvy = b.v.y-a.v.y;
-  // original logic for posterity
-    // var vdif = Math.sqrt(dvx*dvx+dvy*dvy);  // relative velocity
-    // var vdot = 0;                           // if relative velocity==0, no Derivative term for our PiD
-    // if (vdif) {
-      // var uvx = dvx / vdif;                 // velocity unit vector (direction of rel vel)
-      // var uvy = dvy / vdif;
-      // vdot = uvx*udx + uvy*udy;             // dot product of positional direction vs rel vel - we only
-    // }                                       // want to adjust velocity along axis aligned with 2 particles,
-    // var dterm = vdif * vdot * D;        // Derivative term
-  
-  // equivalent yet optimized:
     var vcomp = dvx*udx + dvy*udy;
     var dterm = vcomp * D;        // Derivative term
-  
+
     var xswap = (pterm + dterm) * udx;          // along axis a--b
     var yswap = (pterm + dterm) * udy;
-  
+
     a.f.x += xswap;                          // swap momenta
     b.f.x -= xswap;
     a.f.y += yswap;
