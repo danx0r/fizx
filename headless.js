9c17830655bb59e538e8e20ebc0cddb33cf16132
danx0r <dan.miller@eye0.com>
2022-08-06 13:28:45 -0700

    Start fresh

--- headless.js
+++
@@ -1,8 +0,0 @@
-require('./fizx.js');
-require('./ball16.js');
-require('./ball23.js');
-require('./test.js');
-require('./delaunay.js');
-require('./display_null.js');
-
-first_run();
--- servit.py
+++
@@ -1,8 +0,0 @@
-import SimpleHTTPServer
-import SocketServer
-PORT = 8002
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-SocketServer.TCPServer.allow_reuse_address = True
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-print "serving at port", PORT
-httpd.serve_forever()
